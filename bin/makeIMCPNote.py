8fdf64b77318392338d121416390d343385614c2
Lori Corbani <lori.corbani@jax.org>
2026-05-22 10:20:48 -0400

    wts2-1857/Allele Molecular IMPC Note Load

--- bin/makeIMCPNote.py
+++ bin/makeIMCPNote.py
@@ -268,7 +268,7 @@ def processFile():
 
         # if no errors, process the allele
 
-        note += ' (<A href=http://www.informatics.jax.org/accession/J:384794>J:384794</A>)'
+        note += ' (<A href=https://www.informatics.jax.org/reference/J:384794>J:384794</A>)'
         noteFile.write('%s|%s|%s|%s|%s|%s|%s|%s|%s\n' \
             % (noteKey, alleleKey, mgiTypeKey, noteTypeKey, \
                note, createdByKey, createdByKey, loaddate, loaddate))
